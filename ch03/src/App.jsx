78e8a175b7a26301d8497e35deb2cc48c40f6e9a
JangHwanPark <wkd5878@gmail.com>
2024-04-20 14:53:37 +0900

    feat: Like Button (좋아요 갯수 추가)

--- ch03/src/App.jsx
+++ ch03/src/App.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import {Outlet} from "react-router-dom";
 import Footer from "./layout/Footer";
 import Header from "./layout/Header";
-import Categorise from "./components/Categorise/Categorise";
-import Title from "./components/Title/Title";
 
 function App() {
     return (
         <main className="h-full px-5 antialiased grid gap-12 mt-4 overflow-hidden sm:mx-auto sm:max-w-2xl sm:px-8 md:max-w-6xl md:overflow-visible lg:px-0">
             <Header/>
-            <Title/>
-            <Categorise/>
             <Outlet/>
             <Footer/>
         </main>
--- ch03/src/pages/Home.jsx
+++ ch03/src/pages/Home.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import GridLayout from "../layout/GridLayout";
 import SideMenu from "../components/SideMenu/SideMenu";
 import PostsList from "../components/PostsList/PostsList";
+import Categorise from "../components/Categorise/Categorise";
+import Title from "../components/Title/Title";
 
 export default function Home() {
     return (
-        <GridLayout>
-            <PostsList/>
-            <SideMenu/>
-        </GridLayout>
+        <>
+            <Title/>
+            <Categorise/>
+            <GridLayout>
+                <PostsList/>
+                <SideMenu/>
+            </GridLayout>
+        </>
     );
 }
