e8157b9eb741bd855c73230252013b2e48cc0665
JangHwanPark <wkd5878@gmail.com>
2024-04-20 23:16:02 +0900

    feat: 마크다운 게시글 출력 (posts 내 게시물 PostsList.jsx 컴포넌트 내 출력)

--- ch03/src/components/PostsList/PostsList.jsx
+++ ch03/src/components/PostsList/PostsList.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import LikeButton from "../LikeButton/LikeButton";
 import {Link} from "react-router-dom";
+import { marked } from 'marked';
 
 export default function PostsList() {
+    const [posts, setPosts] = React.useState([]);
+
+    useEffect(() => {
+        // 마크다운 파일 목록 가져옴
+        const markdownFile = require.context('../../posts', true, /\.md$/);
+
+        Promise.all(
+            markdownFile.keys().map(file => {
+                const markdown = markdownFile(file);
+
+                return fetch(markdown)
+                    .then(res => res.text())
+                    .then((text) => ({
+                        slug: file.replace('./', '').replace('.md', ''),
+                        title: text.split('\n')[0].replace('# ', ''), // 첫 번째 줄이 제목
+                        content: marked(text),
+                    }));
+            })
+        ).then((posts) => setPosts(posts));
+    }, []);
+
     return (
-        <article className="p-4 shadow-lg col-span-3">
-            <h2 className="font-semibold text-xl mb-2">
-                <Link to={'/posts/1'}>주요 기사 제목</Link>
-            </h2>
-            <p>기사 내용...</p>
-            <LikeButton/>
-        </article>
+        <>
+            {posts.map((post) => (
+                <article className="p-4 shadow-lg col-span-3" key={post.slug}>
+                    <h2 className="font-semibold text-xl mb-2">
+                        <Link to={`/posts/${post.slug}`}>{post.title}</Link>
+                    </h2>
+                    <p>{post.content}</p>
+                    <LikeButton/>
+                </article>
+            ))}
+        </>
     );
 }
