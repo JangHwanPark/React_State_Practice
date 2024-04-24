5420fc4d78baddb4e9655722ec8ad5c6836b8701
JangHwanPark <wkd5878@gmail.com>
2024-04-24 21:56:19 +0900

    Study: ts Layout(Flex)

---
+++ ts_layout/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './scss/_base.scss';
+import {Outlet} from "react-router-dom";
+
+function App() {
+  return <Outlet/>
+}
+
+export default App;
---
+++ ts_layout/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import WrapLayout from "../layout/WrapLayout";
+
+export default function Home() {
+    return (
+        <WrapLayout>
+            <main>Hone</main>
+        </WrapLayout>
+    );
+}
