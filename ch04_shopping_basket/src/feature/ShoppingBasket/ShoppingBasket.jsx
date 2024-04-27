5f8ac543d46f545442fff89b0c0c212d93cc7f30
JangHwanPark <wkd5878@gmail.com>
2024-04-27 22:16:43 +0900

    fix: 첫번째 데이터 패칭 실패 : products = undefined 문제 해결 (데이터가 아직 로드되지 않았을 때 로딩 표시)

--- ch04_shopping_basket/src/feature/ShoppingBasket/ShoppingBasket.jsx
+++ ch04_shopping_basket/src/feature/ShoppingBasket/ShoppingBasket.jsx
@@ -8,13 +8,18 @@ export default function ShoppingBasket() {
     // 첫번째 데이터 패칭 실패 : products = undefined
     const url = "https://janghwanpark.github.io/data-storage/json-data/product-data/shop-products.json";
     const {products} = useFetch(url);
+
+    // 첫번째 데이터 패칭 실패 : products = undefined 문제 해결
+    // 데이터가 아직 로드되지 않았을 때 로딩 표시
+    if (!products) return <div>isNot Data</div>
+
+    // 데이터가 로딩되면 출력
     console.log("products: ", products)
 
     return (
         <ShoppingBasketLayout>
             <BasketSection sectionTitle={'장바구니'} className={'shop-basket'}>
                 <div>상품 목록 표로 출력</div>
-
                 <div>ShoppingBasket Test</div>
                 <div>ShoppingBasket Test</div>
                 <div>ShoppingBasket Test</div>
