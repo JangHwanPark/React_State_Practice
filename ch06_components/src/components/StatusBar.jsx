d348058ddfc21af59dc5a63f9a00fcabdbf55f10
user <wkd5878@gmail.com>
2024-06-27 15:23:28 +0900

    init

--- ch06_components/src/components/StatusBar.jsx
+++
@@ -1,7 +0,0 @@
-import React, {useEffect, useState} from 'react';
-import useOnlineStatus from "../hooks/useOnlineStatus";
-
-export default function StatusBar() {
-    const isOnline = useOnlineStatus();
-    return  <h1>{isOnline ? "✅ Online" : "❌ Disconnected"}</h1>
-}
---
+++ type_user/src/main.tsx
@@ -0,0 +1,38 @@
+// import lib
+import React from 'react';
+import { createRoot } from 'react-dom/client'; // 명명된 내보내기 사용
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// import components
+import App from '../src/components/pages/App.tsx';
+import Home from '../src/components/pages/Home.tsx';
+
+// import css
+import './scss/index.scss';
+
+// Dom 설정
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = createRoot(rootElement); // createRoot 사용
+
+// 리액트 쿼리 설정
+const queryClient = new QueryClient();
+
+// 리액트 라우트 설정
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {index: true, element: <Home/>}
+        ]
+    },
+]);
+
+root.render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    </React.StrictMode>
+);
