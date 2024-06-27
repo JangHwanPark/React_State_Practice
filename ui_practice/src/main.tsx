76a04e74e4213adf6c9e32cf5d8218c187250d4e
user <wkd5878@gmail.com>
2024-06-27 12:34:19 +0900

    사용자 UI 프로젝트 Init

---
+++ ui_practice/src/main.tsx
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom/client';
+import './scss/index.css'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import App from "./App";
+
+// Dom 설정
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement!);
+
+// 리액트 쿼리 설정
+const queryClient = new QueryClient();
+
+// 리액트 라우트 설정
+const router = createBrowserRouter({
+    path: "/",
+    element: <App/>
+})
+
+root.render(
+    <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}/>
+    </QueryClientProvider>
+);
