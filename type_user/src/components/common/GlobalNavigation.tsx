22aa2ee90a9d6e37d1881a33ac14bb912f8a08a6
user <wkd5878@gmail.com>
2024-06-28 00:25:08 +0900

    feat: 반응형 네비게이션 (기본형) 작업중 - 클릭이벤트까지 구현

--- type_user/src/components/common/GlobalNavigation.tsx
+++ type_user/src/components/common/GlobalNavigation.tsx
@@ -3,20 +3,29 @@ import {Link} from "react-router-dom";
 import {CiMenuBurger} from "react-icons/ci";
 
 export default function GlobalNavigation() {
+    // 클릭값 상태 관리
     const [clickBtn, setClickBtn] = useState(false);
+
+    // 클릭 상태 변경 함수
+    const handleClick = () => {
+        setClickBtn(!clickBtn)
+        console.log(clickBtn)
+    }
 
     return (
         <nav className="glb">
             <div className="logo-wrapper">
                 <h2 className="glb-logo">Logo</h2>
+                <div onClick={handleClick}><CiMenuBurger/></div>
             </div>
-            <ul className="glb-list">
+            {/* 상태를 활용한 클래스 변경 */}
+            <ul className={`glb-list ${clickBtn ? 'glb-active' : ''}`}>
                 <li className="glb-item"><Link to={"/"}>Menu</Link></li>
                 <li className="glb-item"><Link to={"/"}>Menu</Link></li>
                 <li className="glb-item"><Link to={"/"}>Menu</Link></li>
                 <li className="glb-item"><Link to={"/"}>Menu</Link></li>
                 <li className="glb-item"><Link to={"/"}>Menu</Link></li>
-                <li className="hamburger"><CiMenuBurger/></li>
+                <li className="hamburger" onClick={handleClick}><CiMenuBurger/></li>
             </ul>
         </nav>
     );
