efab4828292db0798ce12c2eef764fd5c840efde
user <wkd5878@gmail.com>
2024-07-06 08:44:08 +0900

    study: init

---
+++ ch06/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Products from "./components/Products.jsx";
+
+function App() {
+
+  return (
+    <main>
+      {/* 상품 */}
+      <Products/>
+
+      {/* 장바구니 */}
+
+      {/* 장바구니 정보(결제해야할 정보)  */}
+    </main>
+  )
+}
+
+export default App
